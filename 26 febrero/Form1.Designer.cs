dc41a1f0dc03413aedf17ed742f99763a7dc9aa1
JUAN JOSE CARVAJAL <juan.carvajal668@pascualbravo.edu.co>
2025-03-19 17:06:31 -0500

    Add files via upload

--- 26 febrero/Form1.Designer.cs
+++ 26 febrero/Form1.Designer.cs
@@ -32,9 +32,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(game));
             this.TMR_1 = new System.Windows.Forms.Timer(this.components);
             this.OVNI = new System.Windows.Forms.PictureBox();
-            this.BTN_DESPLAZAR = new System.Windows.Forms.Button();
             this.NAVE = new System.Windows.Forms.PictureBox();
-            this.TMR_2 = new System.Windows.Forms.Timer(this.components);
+            this.TMR_Game = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.OVNI)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NAVE)).BeginInit();
             this.SuspendLayout();
@@ -42,7 +41,7 @@
             // TMR_1
             // 
             this.TMR_1.Interval = 1;
-            this.TMR_1.Tick += new System.EventHandler(this.TMR_1_Tick);
+            this.TMR_1.Tick += new System.EventHandler(this.TMR_Game_Tick);
             // 
             // OVNI
             // 
@@ -51,28 +50,13 @@
             this.OVNI.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.OVNI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.OVNI.Image = ((System.Drawing.Image)(resources.GetObject("OVNI.Image")));
-            this.OVNI.Location = new System.Drawing.Point(400, 120);
+            this.OVNI.Location = new System.Drawing.Point(386, 109);
             this.OVNI.Name = "OVNI";
             this.OVNI.Size = new System.Drawing.Size(218, 139);
             this.OVNI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.OVNI.TabIndex = 0;
             this.OVNI.TabStop = false;
-            this.OVNI.Click += new System.EventHandler(this.IMG_1_Click);
-            // 
-            // BTN_DESPLAZAR
-            // 
-            this.BTN_DESPLAZAR.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.BTN_DESPLAZAR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BTN_DESPLAZAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_DESPLAZAR.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.BTN_DESPLAZAR.Image = ((System.Drawing.Image)(resources.GetObject("BTN_DESPLAZAR.Image")));
-            this.BTN_DESPLAZAR.Location = new System.Drawing.Point(850, 359);
-            this.BTN_DESPLAZAR.Name = "BTN_DESPLAZAR";
-            this.BTN_DESPLAZAR.Size = new System.Drawing.Size(130, 43);
-            this.BTN_DESPLAZAR.TabIndex = 1;
-            this.BTN_DESPLAZAR.Text = "INICIAR";
-            this.BTN_DESPLAZAR.UseVisualStyleBackColor = false;
-            this.BTN_DESPLAZAR.Click += new System.EventHandler(this.BTN_DESPLAZAR_Click);
+            this.OVNI.Click += new System.EventHandler(this.TMR_1_Tick);
             // 
             // NAVE
             // 
@@ -88,11 +72,7 @@
             this.NAVE.TabStop = false;
             this.NAVE.Click += new System.EventHandler(this.TMR_1_Tick);
             // 
-            // TMR_2
-            // 
-            this.TMR_2.Tick += new System.EventHandler(this.NAVE_Click);
-            // 
-            // Form1
+            // game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
@@ -100,12 +80,11 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1003, 670);
             this.Controls.Add(this.NAVE);
-            this.Controls.Add(this.BTN_DESPLAZAR);
             this.Controls.Add(this.OVNI);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
+            this.Name = "game";
             this.Text = "Form1";
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NAVE_KeyPress);
+            this.Load += new System.EventHandler(this.game_Load);
             ((System.ComponentModel.ISupportInitialize)(this.OVNI)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NAVE)).EndInit();
             this.ResumeLayout(false);
@@ -116,9 +95,8 @@
 
         private System.Windows.Forms.Timer TMR_1;
         private System.Windows.Forms.PictureBox OVNI;
-        private System.Windows.Forms.Button BTN_DESPLAZAR;
         private System.Windows.Forms.PictureBox NAVE;
-        private System.Windows.Forms.Timer TMR_2;
+        private System.Windows.Forms.Timer TMR_Game;
     }
 }
 
--- 26 febrero/Form1.cs
+++ 26 febrero/Form1.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,20 +14,31 @@ namespace _JUEGO_NAVE
     {
         int X = 100;
         bool dir = true;
+        bool leftPressed, rightPressed;
+        private List<PictureBox> bullets = new List<PictureBox>();
+        private int score = 0;
+        private Label lblScore;
 
         public game()
         {
             InitializeComponent();
             this.Load += new EventHandler(form1_load);
-            this.NAVE.KeyPress += new KeyPressEventHandler(NAVE_KeyPress);
+            this.KeyDown += new KeyEventHandler(game_KeyDown);
+            this.KeyUp += new KeyEventHandler(game_KeyUp);
+            TMR_1.Tick += new EventHandler(TMR_1_Tick);
 
+            
+            TMR_1.Interval = 20;
+            TMR_1.Start();
 
-
+            
+            Timer tmrGame = new Timer { Interval = 20 };
+            tmrGame.Tick += new EventHandler(TMR_Game_Tick);
+            tmrGame.Start();
         }
 
         private void TMR_1_Tick(object sender, EventArgs e)
         {
-
             OVNI.Location = new Point(X, 20);
             if (dir)
             {
@@ -38,7 +48,7 @@ namespace _JUEGO_NAVE
             {
                 X -= 10;
             }
-            if (X >= this.Size.Width - OVNI.Width)
+            if (X >= this.ClientSize.Width - OVNI.Width)
             {
                 dir = false;
             }
@@ -46,59 +56,75 @@ namespace _JUEGO_NAVE
             {
                 dir = true;
             }
-
-
-
         }
 
-
-        private void IMG_1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void BTN_DESPLAZAR_Click(object sender, EventArgs e)
-        {
-            TMR_1.Enabled = true;
-            MessageBox.Show(this.OVNI.Size.Width.ToString());
-        }
-
-        private void NAVE_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void form1_load(object sender, EventArgs e)
-        {
-            this.KeyPreview = true;
-        }
-
-        private void NAVE_KeyPress(object sender, KeyPressEventArgs e)
+        private void TMR_Game_Tick(object sender, EventArgs e)
         {
             
             int x = NAVE.Location.X;
-            int y = NAVE.Location.Y;
-            int STEP =10;
-            if (e.KeyChar == 'D' || e.KeyChar=='d')
-            {
-                x += STEP;
-                if (x+NAVE.Width > this.ClientSize.Width)
-                {
-                    x = this.ClientSize.Width - NAVE.Width;
-                }
-            }
-            if (e.KeyChar == 'A' || e.KeyChar == 'a')
-            {
-                x -= STEP;
-                if (x < 0)
-                {
-                    x = 0;
-                }
-            }
-           
+            int STEP = 10;
+            if (leftPressed && x > 0) x -= STEP;
+            if (rightPressed && x + NAVE.Width < this.ClientSize.Width) x += STEP;
+            NAVE.Location = new Point(x, NAVE.Location.Y);
+
             
-            Point p= new Point(x, y);
-            NAVE.Location = p;
+            for (int i = bullets.Count - 1; i >= 0; i--)
+            {
+                bullets[i].Top -= 15;
+                if (bullets[i].Bounds.IntersectsWith(OVNI.Bounds))
+                {
+                    this.Controls.Remove(bullets[i]);
+                    bullets.RemoveAt(i);
+                    score += 10;
+                    lblScore.Text = $"Puntuación: {score}";
+                    OVNI.Location = new Point(100, 20);
+                }
+                else if (bullets[i].Top < 0)
+                {
+                    this.Controls.Remove(bullets[i]);
+                    bullets.RemoveAt(i);
+                }
+            }
         }
 
+     
+
+        private void form1_load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+
+            lblScore = new Label
+            {
+                Location = new Point(10, 10),
+                Text = "Puntuación: 0",
+                AutoSize = true
+            };
+            this.Controls.Add(lblScore);
+        }
+
+        private void game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A) leftPressed = true;
+            if (e.KeyCode == Keys.D) rightPressed = true;
+            if (e.KeyCode == Keys.Space)
+            {
+                
+                PictureBox bullet = new PictureBox
+                {
+                    Size = new Size(5, 10),
+                    BackColor = Color.Red,
+                    Location = new Point(NAVE.Location.X + NAVE.Width / 2 - 2, NAVE.Location.Y - 10)
+                };
+                this.Controls.Add(bullet);
+                bullets.Add(bullet);
+            }
+        }
+
+        private void game_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A) leftPressed = false;
+            if (e.KeyCode == Keys.D) rightPressed = false;
+        }
+        private void game_Load(object sender, EventArgs e) { }
     }
 }
